2ba93a49345d255e87eaf6bec392476887793f38
Sandro Salles <you@example.com>
2019-09-05 14:41:37 -0300

    custom menu

--- src/tinymce-for-contentful.js
+++ src/tinymce-for-contentful.js
@@ -40,17 +40,9 @@ window.contentfulExtension.init(function (api) {
       toolbar: tb,
       menubar: mb,
       menu: {
-        edit: {
-          title: 'Edit',
-          items: 'undo redo | cut copy paste | selectall | searchreplace'
-        },
         insert: {
           title: 'Insert',
           items: 'image link media'
-        },
-        format: {
-          title: 'Format',
-          items: 'bold italic underline | forecolor | removeformat'
         },
         tools: {
           title: 'Tools',
